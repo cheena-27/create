188822616d724809b1135fd72aac72ebfdab68e5
Cheena Ramesh Mehrotra <cheenam@cybage.com>
2021-04-12 17:31:40 +0530

    Add inline css

--- src/App.js
+++ src/App.js
@@ -27,8 +27,8 @@ class App extends Component {
   }
   
   render() {
-   const style {
-    backgroundColor : 'white',
+   const style = {
+    backgroundColor: 'white',
     font: 'inherit',
     border: '1px solid blue',
     padding: '8px',
