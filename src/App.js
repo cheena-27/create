9b243764434e638aa090e5bb55539d00fbb69cb0
Cheena Ramesh Mehrotra <cheenam@cybage.com>
2021-04-12 16:59:22 +0530

    Add app.js style

--- src/App.js
+++ src/App.js
@@ -33,12 +33,13 @@ class App extends Component {
       font: "inherit",
       border: 1px solid blue,
       padding: '8px',
+      cursor :'pointer'
     };
   return (
       <div className="App">
         <h1>Hi, I am a React App</h1>
         <p>This is really working !</p>
-        <button onClick={this.switchNameHandler}>Switch name</button>
+        <button style={style} onClick={this.switchNameHandler}>Switch name</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
         <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies : Working </Person>
         <Person name= {this.state.persons[2].name} age= {this.state.persons[2].age}/>
