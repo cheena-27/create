98be5b243ad88b92a848345d5cfc1c426ebc1e27
Cheena Ramesh Mehrotra <cheenam@cybage.com>
2021-04-23 15:42:46 +0530

    Adduser Component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person.js';
-import Adduser from 'src/components/Users/Adduser';
+import Adduser from '.ss/components/Users/Adduser';
 
 
 class App extends Component {
