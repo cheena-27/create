8101b31d120f65a65ad5eb5303569b29d9554de0
Cheena Ramesh Mehrotra <cheenam@cybage.com>
2021-04-26 12:45:25 +0530

    Adduser Component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person.js';
-//import AddUser from './components/Users/AddUser.js';
+import AddUser from './components/Users/AddUser.js';
 
 
 class App extends Component {
@@ -88,8 +88,8 @@ class App extends Component {
          <p className={classes.join('')}>This is really working !</p>
          <button style={style} onClick={this.togglePersonHandler}>Toggle Persons</button>
          {persons}
-        
-       </div>
+         <AddUser />
+        </div>
        );
     }
  }
