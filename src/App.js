a470b6307dce584c7d3a81059587fed1c7f9ce02
Cheena Ramesh Mehrotra <cheenam@cybage.com>
2021-04-28 18:40:00 +0530

    Multiple component

--- src/App.js
+++ src/App.js
@@ -82,17 +82,16 @@ class App extends Component {
        classes.push('bold'); 
      }
   
-  render() { 
-      return (
-      <>
+   return (
+   
        <div className="App">
        <h1>Hi, I am a React App</h1>
          <p className={classes.join('')}>This is really working !</p>
          <button style={style} onClick={this.togglePersonHandler}>Toggle Persons</button>
          {persons}
-         <AddUser />
-       </div>
-       </>
+     </div>
+       <div><AddUser/></div>
+       
        );
     }
  }
