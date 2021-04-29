4fcfdb85eb0f99ae2cbc43bc432268500d293f1a
Cheena Ramesh Mehrotra <cheenam@cybage.com>
2021-04-29 15:50:54 +0530

    Add folder structure

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,7 @@
 
 import React, { Component } from 'react';
 import './conatiners/App.css';
-import Person from './components/Person/Person.js';
+import Person from '../components/Person/Person.js';
 
 
 
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@
 import React, {Component}  from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './containers/App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
